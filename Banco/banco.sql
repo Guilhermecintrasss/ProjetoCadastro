cbd2258c2ec0be40cd56935d615638e86a352472
Guilhermecintrasss <guilhermecintrass@gmail.com>
2023-05-02 09:34:30 -0300

    banco

--- Banco/banco.sql
+++ Banco/banco.sql
@@ -1,2 +1,11 @@
--- Arquivo de criação
--- do banco de dados
+DROP DATABASE IF EXISTS projeto1;
+CREATE DATABASE projeto1;
+use projeto1;
+
+CREATE TABLE usuario (
+id_usuario int(4) primary key auto_increment,
+nome_usuario varchar(50) not null,
+email_usuario varchar(50) not null,
+fone_usuario varchar(30) null,
+senha_usuario varchar(30) not null
+);
