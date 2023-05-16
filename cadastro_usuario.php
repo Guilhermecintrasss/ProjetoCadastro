3a2c4970a68ac998c1da03d285b98102a097a80d
Guilhermecintrasss <guilhermecintrass@gmail.com>
2023-05-16 11:56:30 -0300

    a

--- cadastro_usuario.php
+++ cadastro_usuario.php
@@ -4,7 +4,12 @@ $nome = $_POST['nome'];
 $email = $_POST['email'];
 $fone = $_POST['fone'];
 $senha = $_POST['senha'];
+$sql = "SELECT email_usuario FROM usuario where email_usuario='$email'";
+$result = mysqli_query($con, $sql);
 
+if(mysqli_num_rows($result)>0) {
+    echo "<h3>E-mail já cadastrado</h3>";
+} else{
 echo "<h1>Dados do usuário</h1>";
 echo "Nome: $nome <br>";
 echo "E-mail: $email <br>";
@@ -13,13 +18,13 @@ echo "Senha: $senha <br>";
 
 $sql = "INSERT INTO usuario (nome_usuario,email_usuario,fone_usuario,senha_usuario)";
 $sql .= "VALUES ('".$nome."','".$email."','".$fone."','".$senha."')";
-
 $result = mysqli_query($con, $sql);
+
 if($result){
     echo "Dados cadastrados com sucesso";}
 else{
     echo "Erro ao tentar cadastrar!";}
-
+}
 
 /*Inserir dados
 INSERT INTO usuario (nome_usuario,fone_usuario,email_usuario,senha_usuario) 
