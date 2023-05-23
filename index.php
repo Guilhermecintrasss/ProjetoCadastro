4f42927468dc655fbc922d3124405af7a0558911
Guilhermecintrasss <guilhermecintrass@gmail.com>
2023-05-23 12:14:33 -0300

    23/05

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php 
+    include('cria_sessao.php');
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,10 +12,22 @@
 </head>
 <body>
     <h3>Página Inicial - Projeto Cadastro IFSP</h3>
+    <?php 
+        if(!empty($_SESSION['login'])){ // caso a sessão esteja diferente de vazia
+            // executa isso.
+            echo "<h3>Ola ".$_SESSION['login']['nome_usuario']."</h3>"; // pega o nome do login
+            echo "<a href='logout.php'>Sair</a>";
+        }
+    ?>
     <hr>
     <ul>
        <li><a href="cadastro_usuario.html">Cadastrar</a></li>
        <li><a href="listar_usuarios.php">Listar</a></li>
+       <?php 
+       if(empty($_SESSION['login'])){ // caso a sessão esteja vazia mostra o botão de login
+       echo "<li><a href='login.html'>Login</a></li>";
+        }
+       ?>
     </ul>
 </body>
 </html>
